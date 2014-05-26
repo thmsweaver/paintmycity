66136c0b643b5ba645bc48aab5003512bb3534f0
thmsweaver <thmsweaver@gmail.com>
2014-05-26 12:48:39 -0400

    user login nearly complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'sessions#new'
+  root 'welcome#index'
 
   resources :users, except: [:index]
   resources :sessions, only: [:create]
